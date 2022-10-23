6fd51eeafd4af53dac08bfc0d81a26d9d3a505d7
RIKO-99 <princericko6@gmal.com>
2022-10-23 12:58:17 -0700

    working on formart specifiers

--- printf.c
+++ printf.c
@@ -10,36 +10,76 @@
  */
 int _printf(const char *format, ...)
 {
-	const char *p, *sval;
-	int ival;
+int _printf(const char *format, ...)
 
-	va_list ap; /*points to each unamed arguments*/
+{
 
-	va_start(ap, format); /*points to first argument*/
+	int (*pfunc)(va_list, flags_t *);
+
+	const char *p;
+
+	va_list arguments;
+
+	flags_t flags = {0, 0, 0};
+
+
+
+	register int count = 0;
+
+
+
+	va_start(arguments, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+
+		return (-1);
 
 	for (p = format; *p; p++)
+
 	{
-		if (*p != '%')
+
+		if (*p == '%')
+
 		{
-			putchar(*p);
-			continue;
-		}
-		switch (*++p)
-		{
-			case 'c':
-				ival = va_arg(ap, int);
-				putchar(ival);
-				ival++;
-				break;
-			case 's':
-				for (sval = va_arg(ap, char *); *sval; sval++)
-					putchar(*sval);
-				break;
-			default:
-				putchar(*p);
-				break;
-		}
+
+			p++;
+
+			if (*p == '%')
+
+			{
+
+				count += _putchar('%');
+
+				continue;
+
+			}
+
+			while (get_flag(*p, &flags))
+
+				p++;
+
+			pfunc = get_print(*p);
+
+			count += (pfunc)
+
+				? pfunc(arguments, &flags)
+
+				: _printf("%%%c", *p);
+
+		} else
+
+			count += _putchar(*p);
+
 	}
-	va_end(ap);
-	return (*p);
+
+	_putchar(-1);
+
+	va_end(arguments);
+
+	return (count);
+
 }
